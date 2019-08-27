77f3e641316f42cbcf8189e7d0b0c7200643a638
ljg <ljg0230@gmail.com>
2019-08-27 22:25:03 +0900

    #6.0

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -31,11 +31,9 @@ export default class extends React.Component {
     let result = null;
     try {
       if (isMovie) {
-        const request = await moviesApi.movieDetail(parsedId);
-        result = request.data;
+        ({data : result} = await moviesApi.movieDetail(parsedId));
       } else {
-        const request = await tvApi.showDetail(parsedId);
-        result = request.data;
+        ({data : result} = await tvApi.showDetail(parsedId));
       }
       console.log(result);
     } catch {
@@ -47,6 +45,7 @@ export default class extends React.Component {
 
   render() {
     const { result, error, loading } = this.state;
+    console.log(result);
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
 }
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,17 @@
-export default () => "Search";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const SearchPresenter = ({ movieResults, tvResults, loading, searchTerm, handleSubmit, error }) =>
+  null;
+
+SearchPresenter.prototype = {
+    movieResults:PropTypes.array,
+    tvResults:PropTypes.array,
+    loading:PropTypes.bool.isRequired,
+    searchTerm:PropTypes.string,
+    handleSubmit:PropTypes.func.isRequired,
+    error:PropTypes.string,
+};
+
+export default SearchPresenter;
